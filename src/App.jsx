dabb4966044baf473a56db11631cd760dcdafd22
tamrat-ake <122815343+tamrat-ake@users.noreply.github.com>
2024-06-14 12:59:02 +0300

    Updated previous commit with private changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import GlobaStyle from "./styles/GlobalStyles";
-import Dashboard from "./pages/Dashboard";
-import BooKings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
-import Settings from "./pages/Settings";
-import Account from "./pages/Account";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./ui/AppLayout";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
-import Booking from "./pages/Booking";
-import Checkin from "./pages/Checkin";
-import ProtectedRoute from "./ui/ProtectedRoute";
-import { DarkModeProvider } from "./context/DarkModeContext";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import GlobaStyle from './styles/GlobalStyles';
+import Dashboard from './pages/Dashboard';
+import BooKings from './pages/Bookings';
+
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+import Booking from './pages/Booking';
+import Checkin from './pages/Checkin';
+import ProtectedRoute from './ui/ProtectedRoute';
+import { DarkModeProvider } from './context/DarkModeContext';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -58,7 +59,7 @@ function App() {
           <Toaster
             position="top-center"
             gutter={12}
-            containerStyle={{ marigin: "8px" }}
+            containerStyle={{ marigin: '8px' }}
             toastOptions={{
               success: {
                 duration: 3000,
@@ -67,11 +68,11 @@ function App() {
                 duration: 5000,
               },
               style: {
-                fontSize: "16px",
-                maxWidth: "500px",
-                padding: "16px 24px",
-                backgroundColor: "var(--color--grey--0)",
-                color: "var(--color--grey--700)",
+                fontSize: '16px',
+                maxWidth: '500px',
+                padding: '16px 24px',
+                backgroundColor: 'var(--color--grey--0)',
+                color: 'var(--color--grey--700)',
               },
             }}
           />
